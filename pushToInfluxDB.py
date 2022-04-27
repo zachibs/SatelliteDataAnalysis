9aee4d274ce0a6de99f7f5d2b6ce500d83f72f84
Zachi <zachibsn@gmail.com>
2022-04-27 13:09:24 +0300

    changed up the gitignore file and modified readme

--- pushToInfluxDB.py
+++ pushToInfluxDB.py
@@ -9,12 +9,13 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
 import socket
 import pandas as pd
+from generateFinalData import generate
 
 
 # Get local ip
 hostname = socket.gethostname()
 IP = f"{socket.gethostbyname(hostname)}:8086"
-
+print(IP)
 # Connect to influxdb using the credentials in the docker-compose file
 client = InfluxDBClient(url=IP, token="123456789", org="influx")
 
@@ -29,14 +30,19 @@ bucket_api.delete_bucket(bucket_api.find_bucket_by_name("check2"))
 
 # Writing dataframe data to the database
 # need more work on filtering what columns are fiters and what are values
+generate()
 data = pd.read_csv("generatedCSVs/GeneratedData.csv")
 data.drop("Unnamed: 0", axis=1, inplace=True)
-min = 60000000000
+three_hours_nanoseconds = 10800000000000
 data["timestamp"] = data["timestamp"].apply(
-    lambda x: pd.to_datetime(x).value - min)
+    lambda x: pd.to_datetime(x).value - three_hours_nanoseconds)
+data["battery_volts"] = data["battery_volts"].apply(
+    lambda x: x/1000)
+data.insert(1, "numindex", data.index)
 data.set_index("timestamp", inplace=True)
 bucket_api.delete_bucket(bucket_api.find_bucket_by_name("dataframe"))
 bucket_api.create_bucket(bucket_name="dataframe")
 write_api.write("dataframe", record=data, data_frame_measurement_name="h2o_feet",
-                data_frame_tag_columns=["msg_index"])
+                data_frame_tag_columns=["numindex"])
+print(data)
 client.close()
